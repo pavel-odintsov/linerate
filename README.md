796a2c45dc23d59afc63a86cac33361f8edc0a5c
Sk1f3r <sk1f3r@gmail.com>
2017-03-06 16:07:37 +0300

    minor style changes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Linerate
-Simple script to calculate an edge above which linerate is possible
+Simple script to calculate an edge above which linerate is possible.
+
+Output:
+helix4_40 | Linerate if packet size is higher than 85
+helix4_42 | Linerate if packet size is higher than 85
+trident_plus | Linerate if packet size is higher than 85
+trident2_50 | Linerate if packet size is higher than 113
+trident2_54 | Linerate if packet size is higher than 92
+trident2_plus | Linerate if packet size is higher than 163
+tomahawk | Linerate if packet size is higher than 87
--- linerate.py
+++ linerate.py
@@ -1,15 +1,19 @@
 #
 '''
-The catalogue of ASIC, currently represents a few basic Broadcom chips.
-Every list contains two numbers: switching capacity in Mbps and forwarding capacity in Mpps.
-item = [ sw-capacity Mbps, fw-capacity Mpps ]
+The catalogue of ASICs, currently represents a few basic Broadcom chips.
+Every item contains two numbers: switching capacity in Mbps and forwarding capacity in Mpps.
+'asicName': ( sw-capacity Mbps, fw-capacity Mpps )
 Add your own to figure out above which packet size linerate is possible.
 '''
-helix4 = [ 104, 156 ] # BCM56342
-trident_plus = [ 640, 960 ] # BCM56846
-trident2 = [ 1280, 1440 ] # BCM56850
-trident2_plus = [ 1280, 1000 ] # BCM56864
-tomahawk = [ 3200, 4700 ] # BCM56960
+asics = {
+    'helix4_40': ( 128, 191), # BCM56340
+    'helix4_42': ( 104, 156 ), # BCM56342
+    'trident_plus': ( 640, 960 ), # BCM56846
+    'trident2_50': ( 1280, 1440 ), # BCM56850
+    'trident2_54': ( 720, 1000 ), # BCM56854
+    'trident2_plus': ( 1280, 1000 ), # BCM56864
+    'tomahawk': ( 3200, 4700 ) # BCM56960
+}
 #
 def calc(asicName):
     '''
@@ -18,17 +22,21 @@ def calc(asicName):
     :param asicName: a name of a list with capacity values
     :return: string with an edge value for packet size
     '''
-    sw_capacity_bytes = (asicName[0] * 1024 * 1024 * 1024) / 8
-    fw_capacity_flat = (asicName[1] * 1024 * 1024)
+    sw_capacity_bytes = (asics.get(asicName)[0] * 1024 * 1024 * 1024) / 8
+    fw_capacity_flat = (asics.get(asicName)[1] * 1024 * 1024)
     for i in reversed(range(64,257)):
         #print('Current packet size is {}'.format(i))
         if sw_capacity_bytes / i > fw_capacity_flat:
-            return 'Linerate if packet size is higher than {}'.format(i)
+            return '{} | Linerate if packet size is higher than {}'.format(asicName, i)
     return 'Something went wrong'
 #
 if __name__ == "__main__":
-    print(calc(helix4))
-    print(calc(trident_plus))
-    print(calc(trident2))
-    print(calc(trident2_plus))
-    print(calc(tomahawk))
+    print(calc('helix4_40'))
+    print(calc('helix4_42'))
+    print(calc('trident_plus'))
+    print(calc('trident2_50'))
+    print(calc('trident2_54'))
+    print(calc('trident2_plus'))
+    print(calc('tomahawk'))
+#
+#  EOF
