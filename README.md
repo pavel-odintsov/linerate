b1302b26b115e2a8dfc9d725c8020cba6508f6a3
Sk1f3r <sk1f3r@gmail.com>
2017-03-09 01:36:54 +0300

    shiny and waxed

--- README.md
+++ README.md
@@ -12,17 +12,17 @@ packet size, but the script result is 227.
 In opposite, the Juniper's presentation of QFX5200 applies that line rate for Broadcom Tomahawk (BCM56960) is possible with packet size
 above 250, but the script result is 186.
 
-The search continues ... *UFO image*
+The search continues ... *an UFO image*
 
 ###Output
 ```
-[  Trident+ BCM56846   ] 1280 Mbps / 960  Mpps | Linerate if packet size is higher than 170
-[  Tomahawk BCM56960   ] 6400 Mbps / 4400 Mpps | Linerate if packet size is higher than 186
-[ Trident II BCM56850  ] 2560 Mbps / 1440 Mpps | Linerate if packet size is higher than 227
-[ Trident II+ BCM56864 ] 2560 Mbps / 1000 Mpps | Linerate if packet size is higher than 327
-[ Trident II BCM56854  ] 1440 Mbps / 1000 Mpps | Linerate if packet size is higher than 184
-[   Helix4 BCM56340    ] 256  Mbps / 191  Mpps | Linerate if packet size is higher than 171
-[   Helix4 BCM56342    ] 208  Mbps / 156  Mpps | Linerate if packet size is higher than 170
+[   Trident II BCM56850   ] 2560 Gbps / 1440  Mpps | Linerate if packet size is higher than 222
+[  Trident II+ BCM56864   ] 2560 Gbps / 1000  Mpps | Linerate if packet size is higher than 320
+[     Helix4 BCM56340     ] 256  Gbps /  191  Mpps | Linerate if packet size is higher than 167
+[     Helix4 BCM56342     ] 208  Gbps /  156  Mpps | Linerate if packet size is higher than 166
+[    Trident+ BCM56846    ] 1280 Gbps /  960  Mpps | Linerate if packet size is higher than 166
+[    Tomahawk BCM56960    ] 6400 Gbps / 4400  Mpps | Linerate if packet size is higher than 181
+[   Trident II BCM56854   ] 1440 Gbps / 1000  Mpps | Linerate if packet size is higher than 180
 ```
 
 ###Links
@@ -31,6 +31,3 @@ The search continues ... *UFO image*
 2. [Mellanox SX-2 vs Broadcom Trident II Performance Evaluation](https://www.mellanox.com/related-docs/products/Tolly-215111-Mellanox-SwitchX-2_Performance.pdf)
 3. [Mellanox Spectrum vs Broadcom Tomahawk Performance Evaluation](http://www.mellanox.com/related-docs/products/tolly-report-performance-evaluation-2016-march.pdf)
 4. [Buffer information for particular models](https://people.ucsc.edu/~warner/buffer.html)
-
-
-_Last outcome changes made: March 8th 13:16 UTC_
--- linerate.py
+++ linerate.py
@@ -1,47 +1,48 @@
 #
+# IMPORT
+from collections import namedtuple
+#
 # CONST
-# ETHERNET_CTRL = 20 # bytes: preamble(7) + SFD(1) + IFG (12)
+#ETHERNET_CTRL = 20 # bytes: preamble(7) + SFD(1) + IFG (12)
 #
 '''
 The catalogue of ASICs, currently represents a few basic Broadcom chips.
-Every item contains two numbers: switching capacity in Mbps and forwarding capacity in Mpps.
-'asicName': ( full duplex sys-throughtput Mbps, fw-capacity Mpps )
+Every item contains two numbers: switching capacity in Gbps and forwarding capacity in Mpps.
+'asicName': ( full duplex sys-throughtput Gbps, fw-capacity Mpps )
 Add your own to figure out above which packet size linerate is possible.
 '''
+values = namedtuple('Asic', 'sys_throughtput fw_capacity')
 asics = {
-    #'custom': ( 0, 0 ),
-    #'Mellanox Spectrum': ( 6400, 4770 ),
-    'Helix4 BCM56340': ( 256, 191),
-    'Helix4 BCM56342': ( 208, 156 ),
-    'Trident+ BCM56846': ( 1280, 960 ),
-    'Trident II BCM56850': ( 2560, 1440 ),
-    'Trident II BCM56854': ( 1440, 1000 ),
-    'Trident II+ BCM56864': ( 2560, 1000 ),
-    'Tomahawk BCM56960': ( 6400, 4400 )
+    #'custom': value( 0, 0 ),
+    #'Mellanox Spectrum': value( 6400, 4770 ),
+    'Helix4 BCM56340': values( 256, 191),
+    'Helix4 BCM56342': values( 208, 156 ),
+    'Trident+ BCM56846': values( 1280, 960 ),
+    'Trident II BCM56850': values( 2560, 1440 ),
+    'Trident II BCM56854': values( 1440, 1000 ),
+    'Trident II+ BCM56864': values( 2560, 1000 ),
+    'Tomahawk BCM56960': values( 6400, 4400 )
 }
 #
-def linerateEdge(asicName):
+def linerate_edge(asic_name):
     '''
     The function to iterate thru packet sizes and check if the amount of packets
     with size in bytes fits in available switching capacity.
-    :param asicName: a name of a list with capacity values
+    :param asic_name: a name of a list with capacity values
     :return: string with an edge value for packet size
     '''
-    throughtput_bytes = (asics.get(asicName)[0] * 1024 * 1024 * 1024) / 8
-    fw_capacity_flat = (asics.get(asicName)[1] * 1024 * 1024)
-    for size in reversed(range(64,512)):
-        #print('Current packet size is {}'.format(size))
-        if throughtput_bytes / size >= fw_capacity_flat:
-        #if size * fw_capacity_flat <= throughtput_bytes:
-            a = asicName.center(22, ' ')
-            b = str(asics.get(asicName)[0]).center(4, ' ')
-            c = str(asics.get(asicName)[1]).center(4, ' ')
-            return '[{}] {} Mbps / {} Mpps | Linerate if packet size is higher than {}'.format(a, b, c, size)
+    sys_throughtput_bytes = (asics[asic_name].sys_throughtput * (10 ** 9)) / 8
+    fw_capacity_flat = (asics[asic_name].fw_capacity * (10 ** 6))
+    for size in range(512, 64, -1):
+        if sys_throughtput_bytes / size >= fw_capacity_flat:
+        #if size * fw_capacity_flat <= sys_throughtput_bytes:
+            return '[{:^25}] {:^4} Gbps / {:^5} Mpps | Linerate if packet size is higher than {}'\
+                .format(asic_name, asics[asic_name].sys_throughtput, asics[asic_name].fw_capacity, size)
     return 'Linerate even at 64 bytes packet size'
 #
 if __name__ == "__main__":
     for asic in asics:
-            print(linerateEdge(asic))
+        print(linerate_edge(asic))
     #print(calc('custom'))
 #
 #  EOF
