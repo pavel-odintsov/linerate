29713cf42100102b1b528678b0790eb3b41256cc
Sk1f3r <sk1f3r@gmail.com>
2017-03-08 14:45:39 +0300

    increase of asics numbers

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ http://sk1f3r.ru/pic/broadcom-strata-xgs.png
 
 **Output**
 ```
-[  Trident+ BCM56846   ] 640  Mbps / 960  Mpps | Linerate if packet size is higher than 85
-[ Trident II+ BCM56864 ] 1280 Mbps / 1000 Mpps | Linerate if packet size is higher than 163
-[ Trident II BCM56854  ] 720  Mbps / 1000 Mpps | Linerate if packet size is higher than 92
-[   Helix4 BCM56342    ] 104  Mbps / 156  Mpps | Linerate if packet size is higher than 85
-[  Tomahawk BCM56960   ] 3200 Mbps / 4700 Mpps | Linerate if packet size is higher than 87
-[   Helix4 BCM56340    ] 128  Mbps / 191  Mpps | Linerate if packet size is higher than 85
-[ Trident II BCM56850  ] 1280 Mbps / 1440 Mpps | Linerate if packet size is higher than 113
+[  Trident+ BCM56846   ] 1280 Mbps / 960  Mpps | Linerate if packet size is higher than 170
+[  Tomahawk BCM56960   ] 6400 Mbps / 4700 Mpps | Linerate if packet size is higher than 174
+[   Helix4 BCM56342    ] 208  Mbps / 156  Mpps | Linerate if packet size is higher than 170
+[ Trident II BCM56854  ] 1440 Mbps / 1000 Mpps | Linerate if packet size is higher than 184
+[ Trident II BCM56850  ] 2560 Mbps / 1440 Mpps | Linerate if packet size is higher than 227
+[   Helix4 BCM56340    ] 256  Mbps / 191  Mpps | Linerate if packet size is higher than 171
+[ Trident II+ BCM56864 ] 2560 Mbps / 1000 Mpps | Linerate if packet size is higher than 327
 ```
--- linerate.py
+++ linerate.py
@@ -1,22 +1,22 @@
 #
 # CONST
-ETHERNET_CTRL = 20 # bytes: preamble(7) + SFD(1) + IFG (12)
+# ETHERNET_CTRL = 20 # bytes: preamble(7) + SFD(1) + IFG (12)
 #
 '''
 The catalogue of ASICs, currently represents a few basic Broadcom chips.
 Every item contains two numbers: switching capacity in Mbps and forwarding capacity in Mpps.
-'asicName': ( sw-capacity Mbps, fw-capacity Mpps )
+'asicName': ( full duplex sw-capacity Mbps, fw-capacity Mpps )
 Add your own to figure out above which packet size linerate is possible.
 '''
 asics = {
     #'custom': ( 0, 0 ),
-    'Helix4 BCM56340': ( 128, 191),
-    'Helix4 BCM56342': ( 104, 156 ),
-    'Trident+ BCM56846': ( 640, 960 ),
-    'Trident II BCM56850': ( 1280, 1440 ),
-    'Trident II BCM56854': ( 720, 1000 ),
-    'Trident II+ BCM56864': ( 1280, 1000 ),
-    'Tomahawk BCM56960': ( 3200, 4700 )
+    'Helix4 BCM56340': ( 256, 191),
+    'Helix4 BCM56342': ( 208, 156 ),
+    'Trident+ BCM56846': ( 1280, 960 ),
+    'Trident II BCM56850': ( 2560, 1440 ),
+    'Trident II BCM56854': ( 1440, 1000 ),
+    'Trident II+ BCM56864': ( 2560, 1000 ),
+    'Tomahawk BCM56960': ( 6400, 4700 )
 }
 #
 def calc(asicName):
@@ -28,7 +28,7 @@ def calc(asicName):
     '''
     sw_capacity_bytes = (asics.get(asicName)[0] * 1024 * 1024 * 1024) / 8
     fw_capacity_flat = (asics.get(asicName)[1] * 1024 * 1024)
-    for size in reversed(range(64,257)):
+    for size in reversed(range(64,512)):
         #print('Current packet size is {}'.format(size))
         if sw_capacity_bytes / size >= fw_capacity_flat:
         #if size * fw_capacity_flat <= sw_capacity_bytes:
